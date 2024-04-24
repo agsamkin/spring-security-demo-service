cb07622dba1f4f0a55bf145e1783723c2bde0091
ags <a.g.samkin@gmail.com>
2024-04-24 13:28:39 +0300

    Initial commit

---
+++ src/main/java/com/example/springsecuritydemoservice/SpringSecurityDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritydemoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecuritydemoservice/SpringSecurityDemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritydemoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
